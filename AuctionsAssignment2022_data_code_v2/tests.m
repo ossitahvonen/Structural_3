009b7afd610701d64d0f1ab1e7318d07c3dfdfb8
Tahvonen <osstahv@ad.helsinki.fi>
2022-03-04 18:34:53 +0200

    test2 added

--- AuctionsAssignment2022_data_code_v2/tests.m
+++ AuctionsAssignment2022_data_code_v2/tests.m
@@ -14,33 +14,35 @@ t=10
 % kissa = ksdensity(inside_kernel,'Bandwidth',h)*(P)/ksdensity(inside_kernel,'Bandwidth',h);
 
 %vector to store values
+resp1 = responses.(1);
+resp2 = responses.(2);
 values = []
 for i=1:400
-    k = kernel_try1(i,t);
+    k = kernel_try1(i,t, resp1, resp2);
     values(end+1) = k(:,1);
     
 end
 
-function [out,z] = kernel_try1(z,t1)
+function [out,z] = kernel_try1(z,t1, respons1, respons2)
     %iqr for silverman
-    iqr_data = iqr(responses.(1));
+    iqr_data = iqr(respons1);
     %silverman h
-    h= 0.9*min(std(responses.(1)),iqr_data/1.34)*numel(responses.(1))^(-1/5);
+    h= 0.9*min(std(respons1),iqr_data/1.34)*numel(respons1)^(-1/5);
     %what goes inside the kernel density function(?)
-    inside_kernel = (responses.(1)-t1)/h;
+    inside_kernel = (respons1-t1)/h;
     %indicator element
     p_ind = 0;
-    if (responses.(1)-responses.(2) < z) 
+    if (respons1-respons2 < z) 
         p_ind = 1;
     end
 
     %calculate the nominator and denominator without sums
     kernel_nom = ksdensity(inside_kernel,'Bandwidth',h)*p_ind;
     kernel_denom = ksdensity(inside_kernel,'Bandwidth',h);
-    %these should then be 5000x1 
+    %these should then be 5000x1 s
     %then sum over the rows
-    kernel_nom_sum = sum(kernel_nom,1);
-    kernel_denom_sum = sum(kernel_nom,1);
+    kernel_nom_sum = sum(kernel_nom,2);
+    kernel_denom_sum = sum(kernel_nom,2);
     out = kernel_nom_sum/kernel_denom_sum
     %the whole kernel
     %ksdensity(inside_kernel,'Bandwidth',h)*p_ind/ksdensity(inside_kernel,'Bandwidth',h);
@@ -66,7 +68,7 @@ end
 % phi = @(x) exp(-.5*x.^2)/sqrt(2*pi);       % Normal Density
 % % Kernel
 % h = std(data)*(4/3/numel(data))^(1/5);     % Bandwidth estimated by Silverman's Rule of Thumb
-% kernel = @(x) mean(phi((x-data)/h)/h);     % Kernel Density
+%kernel = @(x) mean(phi((x-data)/h)/h);     % Kernel Density
 % kpdf = @(x) arrayfun(kernel,x);            % Elementwise application
 % % Plot
 % figure(2), clf, hold on
